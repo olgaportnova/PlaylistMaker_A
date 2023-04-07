6d1172af3fa75fc3b30b72a1d8951d93954eb457
olgaportnova <yep4yep@gmail.com>
2023-04-07 13:04:41 +0300

    Sprint #10, homework, second commit

--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -9,26 +9,47 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.databinding.TrackViewBinding
 
 class TrackAdapter: RecyclerView.Adapter<TrackAdapter.TrackHolder>() {
-    val track1=Track("Smells Like Teen Spirit", "Nirvana","5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg")
-    val track2=Track("Billie Jean", "Michael Jackson","4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg")
-    val track3=Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg")
-    val track4=Track("Whole Lotta Love", "Led Zeppelin","5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg")
-    val track5=Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
-    val trackList=arrayListOf<Track>(track1,
-        track2,
-        track3,
-        track4,
-        track5)
+    val trackList = listOf(
+            Track(
+                "Smells Like Teen Spirit",
+                "Nirvana",
+                "5:01",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+            ),
+            Track(
+            "Billie Jean",
+            "Michael Jackson",
+            "4:35",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
+            ),
+            Track (
+                "Stayin' Alive",
+                "Bee Gees",
+                "4:10",
+                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
+                    ),
+            Track ("Whole Lotta Love",
+                    "Led Zeppelin",
+                    "5:33",
+                    "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
+                    ),
+            Track ("Sweet Child O'Mine",
+                    "Guns N' Roses",
+                    "5:03",
+                    "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
+            )
 
 
 
     class TrackHolder (item: View):RecyclerView.ViewHolder(item) {
+
         val binding = TrackViewBinding.bind(item)
+        val cornerRadius = binding.root.context.resources.getDimensionPixelSize(R.dimen.radius_art_work)
         fun bind (track: Track) = with(binding){
         trackName.text= track.trackName
             artistName.text = track.artistName
             trackTime.text=track.trackTime
-            Glide.with(itemView).load(track.artWorkUrl100).transform(RoundedCorners(2)).placeholder(R.drawable.placeholder_playlist).into(artWork)
+            Glide.with(itemView).load(track.artWorkUrl100).transform(RoundedCorners(cornerRadius)).placeholder(R.drawable.placeholder).into(artWork)
 
         }
     }
